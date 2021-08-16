ac629a6454ef96d39047d729eb3d2bcddff0db8f
Gubanov Ilya <yoyrandao@gmail.com>
2021-08-16 13:49:09 +0300

    depl: added production configs

--- build/tools/encrypt-secret.ps1
+++ build/tools/encrypt-secret.ps1
@@ -5,9 +5,9 @@ param (
 );
 
 $local:key = [Byte[]]($passphrase.PadRight(24).Substring(0, 24).ToCharArray());
-$local:content = Get-Content -Path $secretPath;
+$local:content = Get-Content -Path $secretPath -Raw;
 
-$local:secret = $local:content               |
+$local:secret = $local:content    |
   ConvertTo-SecureString -AsPlainText -Force |
   ConvertFrom-SecureString -Key $local:key   |
   Out-File -FilePath $outPath;
